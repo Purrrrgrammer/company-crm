d0fa75bc5db0eaaccfc4f4c022043c36d07c040c
Anastasiya <miss.levinaa@yandex.ru>
2026-06-23 22:51:43 +0700

    chore: add .gitignore and initial project structure

---
+++ CompanyCRM/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyCRM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ CompanyCRM/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CompanyCRM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
